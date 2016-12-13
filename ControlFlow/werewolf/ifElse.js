94c25e2c079f670248770dc80cf41d826e70b2c3
Fernanda Silva <fernanda.rodrigues.cdc@gmail.com>
2016-12-12 18:02:52 -0800

    Creating a variable and control flow

---
+++ ControlFlow/werewolf/ifElse.js
@@ -0,0 +1,17 @@
+var moonPhase = 'full';
+
+
+switch (moonPhase) {
+  case 'full':
+  console.log('Howwlll!');
+    break;
+  case 'mostly full':
+  console.log('Arms and legs are getting hairier');
+    break;
+case'mostly new':
+  console.log('Back on two feet');
+    break;
+  default:
+  console.log('Invalid moon phase');
+    break;
+}
